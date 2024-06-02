4235919b86eac06774a95ab7b2e485d147c42237
szymon_p <szymon.polanski24@gmail.com>
2024-06-02 22:22:53 +0200

    added q_learning

--- frozen_lake.py
+++ frozen_lake.py
@@ -1,8 +1,3 @@
-import sys
-
-sys.path.append(
-    "c:/users/szymon_pc/appdata/local/programs/python/python37/lib/site-packages"
-)
 import pickle
 
 import gymnasium as gym
@@ -10,60 +5,67 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def run(episodes=15000, render=False):
-    env = gym.make(
-        "FrozenLake-v1",
-        map_name="8x8",
-        is_slippery=False,
-        render_mode="human" if render else None,
-    )
+def run(episodes, is_training=True, render=False):
 
-    q = np.zeros((env.observation_space.n, env.action_space.n))
-    learning_rate = 0.9
-    discount_factor = 0.9
+   env = gym.make('FrozenLake-v1', map_name="8x8", is_slippery=True, render_mode='human' if render else None)
 
-    epsilon = 1
-    epsilon_decay_rate = 0.0001
-    rng = np.random.default_rng()
-    rewards_per_episode = np.zeros(episodes)
+   if(is_training):
+      q = np.zeros((env.observation_space.n, env.action_space.n)) # init a 64 x 4 array
+   else:
+      f = open('frozen_lake8x8.pkl', 'rb')
+      q = pickle.load(f)
+      f.close()
 
-    for i in range(episodes):
-        state = env.reset()[0]
-        terminated = False
-        truncated = False
+   learning_rate_a = 0.9 # alpha or learning rate
+   discount_factor_g = 0.9 # gamma or discount rate. Near 0: more weight/reward placed on immediate state. Near 1: more on future state.
+   epsilon = 1         # 1 = 100% random actions
+   epsilon_decay_rate = 0.0001        # epsilon decay rate. 1/0.0001 = 10,000
+   rng = np.random.default_rng()   # random number generator
 
-        while not terminated and not truncated:
-            if rng.random() < epsilon:
-                action = env.action_space.sample()
-            else:
-                action = np.argmax(q[state, :])
+   rewards_per_episode = np.zeros(episodes)
 
-            new_state, reward, terminated, truncated, _ = env.step(action)
+   for i in range(episodes):
+      state = env.reset()[0]  # states: 0 to 63, 0=top left corner,63=bottom right corner
+      terminated = False      # True when fall in hole or reached goal
+      truncated = False       # True when actions > 200
 
-            q[state, action] = q[state, action] + learning_rate * (
-                reward + discount_factor * np.max(q[new_state, :]) - q[state, action]
-            )
-            state = new_state
+      while(not terminated and not truncated):
+         if is_training and rng.random() < epsilon:
+               action = env.action_space.sample() # actions: 0=left,1=down,2=right,3=up
+         else:
+               action = np.argmax(q[state,:])
 
-        epsilon = max(epsilon - epsilon_decay_rate, 0)
-        if epsilon == 0:
-            learning_rate_a = 0.0001
+         new_state,reward,terminated,truncated,_ = env.step(action)
 
-        if reward == 1:
-            rewards_per_episode[i] = 1
+         if is_training:
+               q[state,action] = q[state,action] + learning_rate_a * (
+                  reward + discount_factor_g * np.max(q[new_state,:]) - q[state,action]
+               )
 
-    env.close()
+         state = new_state
 
-    sum_rewards = np.zeros(episodes)
-    for t in range(episodes):
-        sum_rewards[t] = np.sum(rewards_per_episode[max(0, t - 100) : (t + 1)])
-    plt.plot(sum_rewards)
-    plt.savefig("frozen_lake8x8.png")
+      epsilon = max(epsilon - epsilon_decay_rate, 0)
 
-    f = open("frozen_lake8x8.pkl", "wb")
-    pickle.dump(q, f)
-    f.close()
+      if(epsilon==0):
+         learning_rate_a = 0.0001
 
+      if reward == 1:
+         rewards_per_episode[i] = 1
 
-if __name__ == "__main__":
-    run()
+   env.close()
+
+   sum_rewards = np.zeros(episodes)
+   for t in range(episodes):
+      sum_rewards[t] = np.sum(rewards_per_episode[max(0, t-100):(t+1)])
+   plt.plot(sum_rewards)
+   plt.savefig('frozen_lake8x8.png')
+
+   if is_training:
+      f = open("frozen_lake8x8.pkl","wb")
+      pickle.dump(q, f)
+      f.close()
+
+if __name__ == '__main__':
+   run(15000)
+
+   #run(1000, is_training=True, render=False)
