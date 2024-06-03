60a08b40b20486661902e9140357bf2281afea7f
szymon_p <szymon.polanski24@gmail.com>
2024-06-03 13:20:31 +0200

    added alpha tests

--- frozen_lake.py
+++ frozen_lake.py
@@ -5,20 +5,21 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def run(episodes, is_training=True, render=False):
-
-   env = gym.make('FrozenLake-v1', map_name="8x8", is_slippery=True, render_mode='human' if render else None)
+def run_frozen_lake(name_of_save_file, episodes, seed, legend_label, learning_rate_a = 0.9, discount_factor_g = 0.9, epsilon=1, slippery=False, strategy="greedy", is_training=True, render=False):
+   env = gym.make(
+      'FrozenLake-v1',            
+      map_name="8x8", 
+      is_slippery=slippery, 
+      render_mode='human' if render else None)
+   env.reset(seed=seed)
 
    if(is_training):
       q = np.zeros((env.observation_space.n, env.action_space.n)) # init a 64 x 4 array
    else:
-      f = open('frozen_lake8x8.pkl', 'rb')
+      f = open(name_of_save_file + '.pkl', 'rb')
       q = pickle.load(f)
       f.close()
 
-   learning_rate_a = 0.9 # alpha or learning rate
-   discount_factor_g = 0.9 # gamma or discount rate. Near 0: more weight/reward placed on immediate state. Near 1: more on future state.
-   epsilon = 1         # 1 = 100% random actions
    epsilon_decay_rate = 0.0001        # epsilon decay rate. 1/0.0001 = 10,000
    rng = np.random.default_rng()   # random number generator
 
@@ -30,9 +31,17 @@ def run(episodes, is_training=True, render=False):
       truncated = False       # True when actions > 200
 
       while(not terminated and not truncated):
-         if is_training and rng.random() < epsilon:
+         if strategy == "greedy":
+            if is_training and rng.random() < epsilon:
                action = env.action_space.sample() # actions: 0=left,1=down,2=right,3=up
-         else:
+            else:
+               action = np.argmax(q[state,:])
+
+         elif strategy == "boltzman":
+            if is_training and rng.random() < epsilon:
+               tau = 1.0
+               action = rng.choice(env.action_space.n, p=np.exp(q[state, :] / tau) / np.sum(np.exp(q[state, :] / tau)))
+            else:
                action = np.argmax(q[state,:])
 
          new_state,reward,terminated,truncated,_ = env.step(action)
@@ -57,15 +66,15 @@ def run(episodes, is_training=True, render=False):
    sum_rewards = np.zeros(episodes)
    for t in range(episodes):
       sum_rewards[t] = np.sum(rewards_per_episode[max(0, t-100):(t+1)])
-   plt.plot(sum_rewards)
-   plt.savefig('frozen_lake8x8.png')
+   plt.plot(sum_rewards, label=legend_label)
+   plt.savefig(name_of_save_file + ".png")
 
    if is_training:
-      f = open("frozen_lake8x8.pkl","wb")
+      f = open(name_of_save_file + ".pkl","wb")
       pickle.dump(q, f)
       f.close()
 
-if __name__ == '__main__':
-   run(15000)
+# if __name__ == '__main__':
+   # run("frozen_lake8x8", 10000, 42, is_training=True, render=True, strategy="greedy")
 
    #run(1000, is_training=True, render=False)
